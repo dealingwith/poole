427f43f2e2b189376f7e82999fae56c1bcfdc46a
Daniel Miller <dealingwith@gmail.com>
2015-12-25 21:03:18 -0600

    post and more admin foo

--- _posts/stats.rb
+++ _posts/stats.rb
@@ -1,10 +1,18 @@
 
-require 'open-uri'
-require 'timeout'
-require 'csv'
+@years_hash = Hash.new(0)
+words = 0
 
-years = {}
-
-Dir.glob('*.markdown') do |rb_file|
-  
+Dir.glob('*.markdown') do |md_file|
+  filename = File.basename md_file, '.markdown'
+  year = filename.split('-').first
+  # puts year[2..3].to_i
+  # years[] += 1
+  @years_hash[year[2..3].to_i] += 1
+  f = File.read(md_file)
+  words = words + f.scan(/[[:alpha:]]+/).count
 end
+
+@years_hash.each do |year|
+  puts year
+end
+puts words
